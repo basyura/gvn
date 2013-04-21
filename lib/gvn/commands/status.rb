2e4e7383a890774d318230fbb634acdc638d1bef
basyura <basyura@gmail.com>
2013-04-21 18:40:04 +0900

    how can I judge local svn root?

--- lib/gvn/commands/status.rb
+++ lib/gvn/commands/status.rb
@@ -5,15 +5,20 @@ module Gvn
   class Command < Thor
     desc "status", "show working status"
     def status
-      store = GvnStore.new
-      Context.exec do |rc|
-        `svn status #{rc.path}`.each_line do |line|
-          status = Status.new(line)
-          next if rc.ignore?(status)
-          type = store.staged?(status.path) ? '*' : ' '
-          puts type + line
+      root  = `svn info | grep "URL"`.sub("URL: ","").sub("file://","").chomp
+      #puts Dir.pwd
+      #puts root
+      #if Dir.pwd == root
+        store = GvnStore.new
+        Context.exec do |rc|
+          `svn status #{rc.path}`.each_line do |line|
+            status = Status.new(line)
+            next if rc.ignore?(status)
+            type = store.staged?(status.path) ? '*' : ' '
+            puts type + line
+          end
         end
-      end
+      #end
     end
   end
 end
