58c2f6c7169733ca2c3140ed0943acc06e3e03bd
basyura <basyura@gmail.com>
2013-04-21 14:25:57 +0900

    staged by directory

--- lib/gvn/commands/stage.rb
+++ lib/gvn/commands/stage.rb
@@ -2,15 +2,40 @@
 module Gvn
   class Command < Thor
     desc "stage", "stage files"
-    def stage(file)
+    def stage(file=nil)
       require 'pstore'
-      status = Status.new(`svn status #{file}`)
+
       store = GvnStore.new()
+      # show staged files 
+      unless file
+        store.transaction(true) do |store|
+          (store["stage"] || []).each {|file| puts file}
+        end
+        return
+      end
+      
+      targets = []
+      # stage files under directory
+      if File.directory?(file)
+        rc = Gvnrc.new
+        `svn status #{file}`.each_line do |line|
+          status = Status.new(line)
+          next if rc.ignore?(status)
+          targets << status.path
+        end
+      else
+        # stage file
+        targets << Status.new(`svn status #{file}`).path
+      end
+
+      # stage file
       store.transaction do |store|
-        list = (store["stage"] || []) << status.path
+        list = (store["stage"] || []) + targets
         store["stage"] = list.uniq
       end
-      puts "staged #{status.path}"
+
+      puts "staged files"
+      targets.each {|file| puts "  " + file}
       puts ""
       puts "stage"
       store.transaction(true) do |store|
