8df0108ee7d261be6015c97ab6854d1686f4cb84
basyura <basyura@gmail.com>
2013-04-21 12:15:00 +0900

    refactored

--- lib/gvn.rb
+++ lib/gvn.rb
@@ -1,4 +1,11 @@
 require "gvn/version"
+require 'thor'
+require 'yaml'
+
+require 'gvn/command'
+require 'gvn/status'
+require 'gvn/gvnrc'
+require 'gvn/context'
 
 module Gvn
   # Your code goes here...
--- lib/gvn/command.rb
+++ lib/gvn/command.rb
@@ -1,10 +1,11 @@
-# require commands
-Dir.glob(File.dirname(__FILE__) + "/commands/*.rb") do |f|
-  require 'gvn/commands/' + File.basename(f).sub("\.rb", "")
-end
 
 module Gvn
   # define common methods
   class Command < Thor
   end
+end
+
+# require commands
+Dir.glob(File.dirname(__FILE__) + "/commands/*.rb") do |f|
+  require 'gvn/commands/' + File.basename(f).sub("\.rb", "")
 end
